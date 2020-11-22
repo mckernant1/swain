794f2af0f2ce87a4879054a87c23ca15d0602ed0
Tom McKernan <tmeaglei@gmail.com>
2020-11-21 17:28:46 -0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+
+}
+
+group = "com.github.mckernant1.lol.swain"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
