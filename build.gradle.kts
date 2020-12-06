8fff030c3bd8e5b56630b5d10dc280b252c5a563
Tom McKernan <tmeaglei@gmail.com>
2020-12-06 15:24:40 -0800

    adding fatjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     application
     kotlin("jvm") version "1.4.20"
     kotlin("plugin.serialization") version "1.4.20"
+    id("com.github.johnrengelman.shadow") version "5.1.0"
+
 }
 
 group = "com.github.mckernant1.lol.swain"
@@ -27,4 +31,20 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.11.1")
     implementation("org.apache.logging.log4j:log4j-core:2.11.1")
 
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+
+tasks.withType<ShadowJar>() {
+    manifest {
+        attributes["Main-Class"] = "RunnerKt"
+    }
 }
